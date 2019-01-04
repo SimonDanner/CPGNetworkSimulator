7bc0eef0f6857d03b137381e202abab43acb962c
SimonDanner <simon.danner@gmail.com>
2019-01-04 11:00:26 -0500

    added license headers

--- include/CPGNetworkSimulator.hpp
+++ include/CPGNetworkSimulator.hpp
@@ -1,8 +1,19 @@
-//
-//  CPGNetworkSimulator.hpp
-//
-//  Created by Simon Danner on 29/06/2017.
-//
+/*  CPGNetworkSimulator.hpp
+    Copyright (C) 2019  Simon Danner
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #ifndef CPGNetworkSimulator_hpp
 #define CPGNetworkSimulator_hpp
--- src/CPGNetworkSimulator.cpp
+++ src/CPGNetworkSimulator.cpp
@@ -1,10 +1,19 @@
-//
-//  Solver.cpp
-//  aoi2limb
-//
-//  Created by Simon Danner on 29/06/2017.
-//  Copyright © 2017 Simon Danner. All rights reserved.
-//
+/*  CPGNetworkSimulator.cpp: C++ interface for simulator
+    Copyright (C) 2019  Simon Danner
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #include "CPGNetworkSimulator.hpp"
 
